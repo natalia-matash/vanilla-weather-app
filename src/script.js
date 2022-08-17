2ada71a03727ec96969c195c33b7d24653118228
Nataliyayaya <natalimatash@gmail.com>
2022-08-17 22:44:14 +0300

    Updared the time

--- src/script.js
+++ src/script.js
@@ -1,3 +1,17 @@
+function formatDate(timestamp) {
+   let date = new Date(timestamp);
+   let days = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+   let day = days[date.getDay()];
+   let hours = date.getHours();
+   if (hours < 10) {
+      hours =`0${hours}`};
+   let minutes = date.getMinutes();
+   if (minutes < 10) {
+      minutes =`0${hours}`};
+   return `${day} ${hours}:${minutes}`;
+
+}
+
 function displayTemperature(response) {
    let cityElement = document.querySelector("#city");
    cityElement.innerHTML = response.data.name;
@@ -9,6 +23,8 @@ function displayTemperature(response) {
    humidityElement.innerHTML = response.data.main.humidity;
    let windElement = document.querySelector("#wind");
    windElement.innerHTML = response.data.wind.speed;
+   let dateElement = document.querySelector("#date");
+   dateElement.innerHTML = formatDate(response.data.dt * 1000);
    }
 
 let apiKey = "0511a6e92a8692a228d7c70698a18f5d";
